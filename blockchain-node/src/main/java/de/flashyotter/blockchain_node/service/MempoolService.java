4df3b701dcad715af2d33d1175a40ed4b387fb64
masssi164 <95998874+masssi164@users.noreply.github.com>
2025-06-30 18:38:06 +0200

    configurable mempool size

--- blockchain-node/src/main/java/de/flashyotter/blockchain_node/service/MempoolService.java
+++ blockchain-node/src/main/java/de/flashyotter/blockchain_node/service/MempoolService.java
@@ -3,6 +3,7 @@ package de.flashyotter.blockchain_node.service;
 import blockchain.core.mempool.Mempool;
 import blockchain.core.model.Transaction;
 import blockchain.core.model.TxOutput;
+import de.flashyotter.blockchain_node.config.NodeProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +16,11 @@ import java.util.Map;
 @Service
 public class MempoolService {
 
-    private final Mempool mempool = new Mempool();
+    private final Mempool mempool;
+
+    public MempoolService(NodeProperties props) {
+        this.mempool = new Mempool(props.getMempoolMaxSize());
+    }
 
     public void submit(Transaction tx, Map<String, TxOutput> utxo) {
         mempool.add(tx, utxo);
